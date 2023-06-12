766f3d9a16cde2874ca9ec02a72f49f793c06782
freshwaterlake <srinivas.peeta@univariety.com>
2023-06-12 12:33:40 +0530

    library structure change

---
+++ demo-project/src/features/demo-page/demo-page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextFormControl } from "../../../../navrang-component-library/src/controls/form-controls/TextFormControl";
+
+const DemoPage = () => {
+    return (
+        <div>
+            <TextFormControl />
+        </div>
+    );
+};
+
+export default DemoPage;
