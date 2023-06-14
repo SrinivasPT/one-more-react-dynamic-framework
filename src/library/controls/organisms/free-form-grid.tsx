41b106cd3792c999b6f039d306afab6be41fa007
srinivaspt <srinivaspt@outlook.com>
2023-06-15 01:50:23 +0530

    minor css changes for free-form

--- src/library/controls/organisms/free-form-grid.tsx
+++ src/library/controls/organisms/free-form-grid.tsx
@@ -31,17 +31,23 @@ const FreeFormGrid: React.FC<ControlProps> = ({ control, dataKey: propsDataKey }
     return (
         <div className="row">
             {data.map((item: any, index: number) => (
-                <div key={`${dataKey}-${index}`} className={`row ${control?.className}`}>
-                    <div className="col-md-11">
-                        <SectionBuilder control={control} dataKey={`${dataKey}.[${index}]`} />
-                    </div>
-                    <div className="col-md-1">
-                        <i className="bi bi-trash fs-3 text-danger" role="button" onClick={(event) => handleDelete(event, index)}></i>
+                <div key={`${dataKey}-${index}`} className={`row ${control?.className} mb-3`}>
+                    <div className="d-flex">
+                        <div className="flex-grow-1">
+                            <SectionBuilder control={control} dataKey={`${dataKey}.[${index}]`} />
+                        </div>
+                        <div className="m-2">
+                            <i className="bi bi-trash fs-3 text-danger" role="button" onClick={(event) => handleDelete(event, index)}></i>
+                        </div>
                     </div>
                 </div>
             ))}
-            <div className="col-md-11 text-end">
-                <i className="bi bi-plus-circle fs-3 text-danger" role="button" onClick={handleAdd}></i>
+            <div className="col-md-12 d-flex justify-content-end">
+                <div className="input-group mt-3">
+                    <button className="btn btn-light fs-5 text-danger" onClick={handleAdd}>
+                        <i className="bi bi-plus-circle me-2"></i> <span>Add</span>
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/library/layout/card-layout.tsx
+++ src/library/layout/card-layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CardLayout = (props: any) => {
     const { control } = props;
     return (
-        <div className="card">
+        <div className="card mb-3 mt-3">
             <div className="card-header text-uppercase fw-bold">{control.title}</div>
             <div className="card-body">{props.children}</div>
         </div>
